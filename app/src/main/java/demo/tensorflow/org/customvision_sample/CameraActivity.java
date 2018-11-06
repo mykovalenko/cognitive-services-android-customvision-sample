566625a19726a32263e7fe6621803dde6861f868
mykovalenko <mykovalenko@futurica.es>
2018-11-06 12:16:28 +0100

    Update CameraActivity.java

--- app/src/main/java/demo/tensorflow/org/customvision_sample/CameraActivity.java
+++ app/src/main/java/demo/tensorflow/org/customvision_sample/CameraActivity.java
@@ -328,12 +328,7 @@ public abstract class CameraActivity extends Activity implements OnImageAvailabl
       camera2Fragment.setCamera(cameraId);
       fragment = camera2Fragment;
     } else {
-      //fragment = new LegacyCameraConnectionFragment(this, getLayoutId());
-        fragment = new LegacyCameraConnectionFragment();
-        Bundle bundle = new Bundle();
-        bundle.putSerializable("me", this);
-        bundle.putInt("lid", getLayoutId());
-        fragment.setArguments(bundle);
+      fragment = new LegacyCameraConnectionFragment(this, getLayoutId());
     }
 
     getFragmentManager()
